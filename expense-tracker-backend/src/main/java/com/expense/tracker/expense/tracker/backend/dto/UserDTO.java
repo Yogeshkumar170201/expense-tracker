a2730b79354bff08e3db1987afa1464af9fb7d4d
Yogesh Kumar <yogeshkumar@CORPLC9151525.local>
2023-11-12 17:50:16 +0530

    Backened

---
+++ expense-tracker-backend/src/main/java/com/expense/tracker/expense/tracker/backend/dto/UserDTO.java
@@ -0,0 +1,53 @@
+package com.expense.tracker.expense.tracker.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+public class UserDTO implements UserDetails {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+}
--- expense-tracker-frontend/app/incomes/page.tsx
+++ expense-tracker-frontend/app/incomes/page.tsx
@@ -1,6 +1,40 @@
+"use client"
+
 import Sidebar from "../components/Sidebar"
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import { useState } from "react";
 
 const Incomes = () => {
+
+  const [incomeInput, setIncomeInput] = useState({
+    "title":"",
+    "amount":"",
+    "date":"",
+    "category":"Salary",
+    "reference":""
+  })
+
+  const handleChange = (e: { target: { name: String; value: String; }; })=>{
+    setIncomeInput({ ...incomeInput, [e.target.name as unknown as string]: e.target.value })
+  }
+
+  const submitIncomeInput = ()=>{
+    console.log(incomeInput)
+    
+    setIncomeInput(
+      {
+        "title":"",
+        "amount":"",
+        "date":"",
+        "category":"Salary",
+        "reference":""
+      }
+    )
+  }
+
   return (
     <div className='flex flex-row justify-between p-[30px] flex-wrap bg-gradient-to-r from-[#ded3ed] to-[#8a6bb5]'>
       <Sidebar/>
@@ -9,29 +43,113 @@ const Incomes = () => {
         <p className="text-center font-bold border-[8px] border-[#b9cceb] rounded-[20px] p-[10px]">Total Income : <span className="text-[#82BB2E]">₹1600</span></p>
         <div className="flex flex-row justify-between">
           <div className="w-[35%] flex flex-col justify-between gap-y-[15px]">
-            <input placeholder="Salary Title" type="text" className="p-[10px] text-[20px] rounded-[10px] rounded-[10px] border-[4px] border-[#b9cceb]"/>
-            <input placeholder="Salary Amount" type="text" className="p-[10px] text-[20px] rounded-[10px] border-[4px] border-[#b9cceb]"/>
-            <input placeholder="Enter a Date" type="date" className="p-[10px] text-[20px] rounded-[10px] border-[4px] border-[#b9cceb]"/>
-            <select className="p-[10px] text-[20px] rounded-[10px] border-[4px] border-[#b9cceb]">
-              <option value="1">Salary</option>
-              <option value="2">Freelancing</option>
-              <option value="3">Investments</option>
-              <option value="4">Stocks</option>
-              <option value="5">Bitcoin</option>
-              <option value="6">Bank Transfer</option>
-              <option value="7">Youtube</option>
-              <option value="8">Others</option>
+            <input placeholder="Salary Title" type="text" className="p-[10px] text-[20px] rounded-[10px] rounded-[10px] border-[4px] border-[#b9cceb]" name="title" value={incomeInput.title} onChange={handleChange} />
+            <input placeholder="Salary Amount" type="text" className="p-[10px] text-[20px] rounded-[10px] border-[4px] border-[#b9cceb]" name="amount" value={incomeInput.amount} onChange={handleChange}/>
+            <input placeholder="Enter a Date" type="date" className="p-[10px] text-[20px] rounded-[10px] border-[4px] border-[#b9cceb]" name="date" value={incomeInput.date} onChange={handleChange}/>
+            <select className="p-[10px] text-[20px] rounded-[10px] border-[4px] border-[#b9cceb]" name="category" value={incomeInput.category} onChange={handleChange}>
+              <option value="Salary">Salary</option>
+              <option value="Freelancing">Freelancing</option>
+              <option value="Investments">Investments</option>
+              <option value="Stocks">Stocks</option>
+              <option value="Bitcoin">Bitcoin</option>
+              <option value="Bank Transfer">Bank Transfer</option>
+              <option value="Youtube">Youtube</option>
+              <option value="Others">Others</option>
             </select>
-            <textarea name="reference" placeholder="Enter a Reference" className="p-[10px] text-[20px] rounded-[10px] w-full h-[150px] resize-none border-[4px] border-[#b9cceb]">
+            <textarea placeholder="Enter a Reference" className="p-[10px] text-[20px] rounded-[10px] w-full h-[150px] resize-none border-[4px] border-[#b9cceb]" name="reference" value={incomeInput.reference} onChange={handleChange}>
             </textarea>
-            <button className="p-[10px] text-[20px] rounded-[30px] bg-[#f294ad] text-white font-bold"> + Add Income</button>
+            <button className="p-[10px] text-[20px] rounded-[30px] bg-[#f294ad] text-white font-bold hover:bg-[#ed668a]" onClick={submitIncomeInput}> + Add Income</button>
           </div>
-          <div>
-            <ul>
-              <li>l1</li>
-              <li>l2</li>
-              <li>l3</li>
-              <li>l4</li>
+          <div className="w-[63%]">
+            <ul className="flex flex-col gap-y-[20px]">
+              <li className=" flex flex-row justify-between items-center p-[20px] w-full border-[4px] border-[#b9cceb] h-[100px] rounded-[20px] bg-[#FFFFFF]">
+                <div className="p-[5px]">
+                  <PaymentsIcon className="w-10 h-10"/>
+                </div>
+                <div className="flex flex-col justofy-between items-center text-[20px] gap-y-[12px]">
+                  <p className="text-[#81BA2E] font-bold">Developer Salary</p>
+                  <div className="flex flex-row justify-between items-center gap-x-[30px]">
+                    <p>₹ 1600</p>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <CalendarTodayIcon/> 
+                      <p>25/11/2023</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <ChatBubbleIcon/>
+                      <p>This is monthly salary</p>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <DeleteRoundedIcon className="w-10 h-10"/>
+                </div>
+              </li>
+              <li className=" flex flex-row justify-between items-center p-[20px] w-full border-[4px] border-[#b9cceb] h-[100px] rounded-[20px] bg-[#FFFFFF]">
+                <div className="p-[5px]">
+                  <PaymentsIcon className="w-10 h-10"/>
+                </div>
+                <div className="flex flex-col justofy-between items-center text-[20px] gap-y-[12px]">
+                  <p className="text-[#81BA2E] font-bold">Developer Salary</p>
+                  <div className="flex flex-row justify-between items-center gap-x-[30px]">
+                    <p>₹ 1600</p>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <CalendarTodayIcon/> 
+                      <p>25/11/2023</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <ChatBubbleIcon/>
+                      <p>This is monthly salary</p>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <DeleteRoundedIcon className="w-10 h-10"/>
+                </div>
+              </li>
+              <li className=" flex flex-row justify-between items-center p-[20px] w-full border-[4px] border-[#b9cceb] h-[100px] rounded-[20px] bg-[#FFFFFF]">
+                <div className="p-[5px]">
+                  <PaymentsIcon className="w-10 h-10"/>
+                </div>
+                <div className="flex flex-col justofy-between items-center text-[20px] gap-y-[12px]">
+                  <p className="text-[#81BA2E] font-bold">Developer Salary</p>
+                  <div className="flex flex-row justify-between items-center gap-x-[30px]">
+                    <p>₹ 1600</p>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <CalendarTodayIcon/> 
+                      <p>25/11/2023</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <ChatBubbleIcon/>
+                      <p>This is monthly salary</p>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <DeleteRoundedIcon className="w-10 h-10"/>
+                </div>
+              </li>
+              <li className=" flex flex-row justify-between items-center p-[20px] w-full border-[4px] border-[#b9cceb] h-[100px] rounded-[20px] bg-[#FFFFFF]">
+                <div className="p-[5px]">
+                  <PaymentsIcon className="w-10 h-10"/>
+                </div>
+                <div className="flex flex-col justofy-between items-center text-[20px] gap-y-[12px]">
+                  <p className="text-[#81BA2E] font-bold">Developer Salary</p>
+                  <div className="flex flex-row justify-between items-center gap-x-[30px]">
+                    <p>₹ 1600</p>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <CalendarTodayIcon/> 
+                      <p>25/11/2023</p>
+                    </div>
+                    <div className="flex flex-row justify-between items-center gap-x-[5px]">
+                      <ChatBubbleIcon/>
+                      <p>This is monthly salary</p>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <DeleteRoundedIcon className="w-10 h-10"/>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
