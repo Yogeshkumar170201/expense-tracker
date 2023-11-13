69e80b40672842328e4ef74c4f25dc618b893ce0
Yogesh Kumar <yogeshkumar@CORPLC9151525.local>
2023-11-14 01:34:33 +0530

    Backend

--- expense-tracker-backend/src/main/java/com/expense/tracker/expense/tracker/backend/models/User.java
+++ expense-tracker-backend/src/main/java/com/expense/tracker/expense/tracker/backend/models/User.java
@@ -1,21 +1,5 @@
 package com.expense.tracker.expense.tracker.backend.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import lombok.Builder;
-import lombok.Data;
 
-@Entity
-@Data
-@Builder
-@Table(name = "user_details")
 public class User {
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String name;
-    private String email;
-    private String password;
-    private Boolean isEnabled;
 }
